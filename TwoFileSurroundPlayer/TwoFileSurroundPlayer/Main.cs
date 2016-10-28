dc712e582c6ba46820aac046107fd049013c70b0
Benji <dothaler@gmail.com>
2016-10-28 23:17:14 +0200

    Init
    Initial Work, Imported NAudio, added WaveClient etc

---
+++ TwoFileSurroundPlayer/TwoFileSurroundPlayer/Main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TwoFileSurroundPlayer
+{
+    static class CMain
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+            var client = GetWaveClient();
+            //client.Initialize();
+            
+        } 
+
+        public static NAudio.CoreAudioApi.AudioClient GetWaveClient()
+        {
+            Wave wave = new Wave();
+            return wave.Client;
+        }
+    }
+    
+
+}
---
+++ TwoFileSurroundPlayer/TwoFileSurroundPlayer/MainWindows.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoFileSurroundPlayer
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            NAudio.Gui.VolumeMeter meter = new NAudio.Gui.VolumeMeter();
+
+            meter.Paint += new PaintEventHandler(this.MainWindow_Paint);
+
+        }
+
+        private void MainWindow_Paint(object sender, PaintEventArgs e)
+        {
+            // Create a local version of the graphics object for the PictureBox.
+            Graphics g = e.Graphics;
+            ImageConverter conv;
+          
+            Point p = e.ClipRectangle.Location;
+            
+
+            //g.DrawImage(i, p);
+            
+            // Draw a line in the PictureBox.
+            g.DrawLine(Pens.Red, Left, Top,
+                Right, Bottom);
+        }
+    }
+}
